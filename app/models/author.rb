a1cdcc7f33cf4b758be03b14d807275100a75d84
otienosteve <steveotieno701@gmail.com>
2022-09-08 14:38:10 +0300

    Complete

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :phone_number, length: {is: 10}
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title , presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction ), message: "is not included in the list" }
+    validate :invalid_title
+
+    def invalid_title
+        if !(title.to_s.include?("Won't Believe"))
+            errors.add(:title,"not click_baitly")
+        elsif title.to_s.include?("Secret") 
+            errors.add(:title,"not click_baitly")
+        elsif title.to_s.include?("Guess")
+            errors.add(:title,"not click_baitly")
+        elsif title.to_s.include?("Top ")
+            errors.add(:title,"not click_baitly")
+        end
+
+    end
 end
